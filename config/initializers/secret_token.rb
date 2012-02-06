5ab2ffe0fc3293b63565806b07e1133701c24c58
Samuel Pozaicer <sam.pz3r@gmail.com>
2012-02-05 22:23:55 -0600

    Fresh Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharelib::Application.config.secret_token = 'd447a1dbb307108ed456a6a6b75fbe7c033245deec859ae23758d2938af3040ad1c84138c1da0983b0580fb23b2051cb2f4a103ab8d04320f2e82b8f5cabdec0'
